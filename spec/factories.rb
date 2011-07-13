21cdc8a8cd6f868a9f1288fa28370d91999f5f22
William <wbiasotti@fairbanks.com>
2011-07-13 13:56:38 -0400

    Added a User factory and a Test for the show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name		     "William Biasotti"
+  user.email                 "wbiasotti@example.com"
+  user.password		     "foobar"
+  user.password_confirmation "foobar"
+end
