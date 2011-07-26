0ddc24be07642134d33b1d9eba204770adf51b7e
William <wbiasotti@fairbanks.com>
2011-07-26 11:05:07 -0400

    Changed the logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
    
    #Return a title on a per-page basis
    def title
-      base_title = "Ruby on Rails Tutorial Sample App"
+      base_title = "Will's Tutorial Website"
       if @title.nil?
 	 base_title
       else
